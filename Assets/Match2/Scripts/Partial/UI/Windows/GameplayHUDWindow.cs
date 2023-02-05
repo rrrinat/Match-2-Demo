5b6a671a637ae2dcde9720b88b23c3459e84fa3f
rrrinat <rrrinat84@gmail.com>
2023-02-06 04:07:59 +0700

    - update (gui kit)

---
+++ Assets/Match2/Scripts/Partial/UI/Windows/GameplayHUDWindow.cs
@@ -0,0 +1,11 @@
+using Match2.Common.UI.Windows;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Match2.Partial.UI.Windows
+{
+    public class GameplayHUDWindow : WindowBase
+    {
+        [SerializeField] private Button settingsButton;
+    }
+}
