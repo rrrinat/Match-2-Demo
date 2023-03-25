d7a0c580cdf830a7547e74b0e9692c5606023228
rrrinat <rrrinat84@gmail.com>
2023-03-26 02:30:30 +0700

    - add (level finished state)

---
+++ Assets/Match2/Scripts/Partial/Gameplay/Level/LevelStates/Factories/LevelFinishedStateFactory.cs
@@ -0,0 +1,20 @@
+﻿using Match2.Common.UI.Windows;
+using Match2.Partial.Gameplay.Factories;
+using Match2.Partial.Gameplay.Level.LevelStates.States;
+
+namespace Match2.Partial.Gameplay.Level.LevelStates.Factories
+{
+    public class LevelFinishedStateFactory
+    {
+        private IFieldFactory fieldFactory;
+        private WindowPresenter windowPresenter;
+        
+        public LevelFinishedStateFactory(IFieldFactory fieldFactory, WindowPresenter windowPresenter)
+        {
+            this.fieldFactory = fieldFactory;
+            this.windowPresenter = windowPresenter;
+        }
+        
+        public LevelFinishedState Create(LevelStateMachine levelStateMachine) => new LevelFinishedState(levelStateMachine, fieldFactory, windowPresenter);
+    }
+}
---
+++ Assets/Match2/Scripts/Partial/Gameplay/Level/LevelStates/States/LevelFinishedState.cs
@@ -0,0 +1,32 @@
+﻿using Match2.Common.UI.Windows;
+using Match2.Partial.Gameplay.Factories;
+using UnityEngine;
+
+namespace Match2.Partial.Gameplay.Level.LevelStates.States
+{
+    public class LevelFinishedState : LevelState
+    {
+        private IFieldFactory fieldFactory;
+        private WindowPresenter windowPresenter;
+        public LevelFinishedState(LevelStateMachine levelStateMachine, IFieldFactory fieldFactory, WindowPresenter windowPresenter) : base(levelStateMachine)
+        {
+            this.fieldFactory = fieldFactory;
+            this.windowPresenter = windowPresenter;
+        }
+
+        public override void Enter()
+        {
+            Debug.Log($"LevelFinishedState");
+        }
+
+        public override void Update()
+        {
+            
+        }
+
+        public override void Exit()
+        {
+            
+        }
+    }
+}
